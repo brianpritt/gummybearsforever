08193e8deca09cbac62bce5f0496af8e3f4008ef
Brian Pritt <brianpritt@gmail.com>
2017-02-09 22:02:30 -0800

    index list and details page added

--- src/gummybearsforever/Controllers/InventoryController.cs
+++ src/gummybearsforever/Controllers/InventoryController.cs
@@ -3,17 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+using GummyBearsForever.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace GummyBearsForever.Controllers
 {
     public class InventoryController : Controller
     {
-        // GET: /<controller>/
+        private GummyBearsForeverDbContext db = new GummyBearsForeverDbContext();
         public IActionResult Index()
         {
-            return View();
+            return View(db.Inventories.ToList());
+            
         }
+        public IActionResult Details(int id)
+        {
+            var thisGummy = db.Inventories
+                .FirstOrDefault(inventories => inventories.GummyId == id);
+            return View(thisGummy);
+       }
+    //    public ActionResult Create()
+    //    {
+    //        ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "LocationName");
+
+    //        ViewBag.MealTypeId = new SelectList(db.MealTypes, "MealTypeId", "MealTypeName");
+    //        return View();
+    //    }
+    //    [HttpPost]
+    //    public ActionResult Create(Experience experience)
+    //    {
+    //        db.Experiences.Add(experience);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+    //    public ActionResult Update(int id)
+    //    {
+    //        var thisExperience = db.Experiences.FirstOrDefault(experience => experience.ExperienceId == id);
+    //        ViewBag.LocationId = new SelectList(db.Locations, "LocationId", "LocationName");
+
+    //        ViewBag.MealTypeId = new SelectList(db.MealTypes, "MealTypeId", "MealTypeName");
+    //        return View(thisExperience);
+    //    }
+    //    [HttpPost]
+    //    public ActionResult Update(Experience experience)
+    //    {
+    //        db.Entry(experience).State = EntityState.Modified;
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+    //    public ActionResult Delete(int id)
+    //    {
+    //        var thisExperience = db.Experiences.FirstOrDefault(experiences => experiences.ExperienceId == id);
+    //        return View(thisExperience);
+    //    }
+
+    //    [HttpPost, ActionName("Delete")]
+    //    public ActionResult DeleteConfirmed(int id)
+    //    {
+    //        var thisExperience = db.Experiences.FirstOrDefault(experiences => experiences.ExperienceId == id);
+    //        db.Experiences.Remove(thisExperience);
+    //        db.SaveChanges();
+    //        return RedirectToAction("Index");
+    //    }
+
+
+
     }
 }
--- src/gummybearsforever/Models/GummyBearsForeverDbContext.cs
+++ src/gummybearsforever/Models/GummyBearsForeverDbContext.cs
@@ -6,14 +6,17 @@ namespace GummyBearsForever.Models
 {
     public class GummyBearsForeverDbContext : DbContext
     {
-        public virtual DbSet<Inventory> Inventtories { get; set; }
+        public virtual DbSet<Inventory> Inventories { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
             options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummyBearsForever;integrated security=True");
         }
 
-        public GummyBearsForeverDbContext(DbContextOptions<GummyBearsForeverDbContext> options)
-            : base(options)
+        public GummyBearsForeverDbContext(DbContextOptions<GummyBearsForeverDbContext> options) : base(options)
+        {
+        }
+
+        public GummyBearsForeverDbContext()
         {
         }
 
