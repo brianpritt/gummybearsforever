f08bfe20fed056af81c95e8c5ae91868b0c0e510
Brian Pritt <brianpritt@gmail.com>
2017-02-09 19:59:47 -0800

    add all migration files and packages

---
+++ src/gummybearsforever/Models/GummyBearsForeverContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace GummyBearsForeverWithMigrations.Models
+{
+    public class GummyBearsForever : DbContext
+    {
+        //public GummyBearsForeverContext()
+        //{
+        //}
+
+        //public DbSet<Category> Categories { get; set; }
+
+        //public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GummyBearsForeverWithMigrations;integrated security=True");
+        }
+
+        //public GummyBearsForeverContext(DbContextOptions<GummyBearsForeverContext> options)
+        //    : base(options)
+        //{
+        //}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
--- src/gummybearsforever/Startup.cs
+++ src/gummybearsforever/Startup.cs
@@ -1,32 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using GummyBearsForever.Models;
 
-namespace gummybearsforever
+
+namespace GummyBearsForever
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFramework()
+                .AddDbContext<GummyBearsForeverContext>(options =>
+                    options.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+        public void Configure(IApplicationBuilder app)
+        {
+
 
             app.Run(async (context) =>
             {
